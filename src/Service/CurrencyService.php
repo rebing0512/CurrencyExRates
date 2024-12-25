a7a96574dd6d9d496506ce0a78c6206009cbc4c0
wangj0 <rebing0512@aliyun.com>
2024-12-25 19:08:28 +0800

    commit:2.0版本,调用alapi.cn接口获取汇率

--- src/Service/CurrencyService.php
+++ src/Service/CurrencyService.php
@@ -28,18 +28,38 @@ class CurrencyService
     private static $url = 'http://www.webmasterhome.cn';
     // 接口地址
     private static $api = '/huilv/huilvchaxun.asp';
+    // 接口域名V2
+    private static $url_v2 = '';
+    // 接口地址V2
+    private static $api_v2 = '';
     // 默认页码
     private static $page = 1;
     // 每页数量
     private static $page_size = 20;
     // 数据库表名
     private static $table = 'currency_code';
+    // token_v2
+    private static $token_v2 = 'MKGEAN3yhsKwyQZT';
+    // 版本
+    private static $version = '2.0';
+
     /**
      * 构造函数
+     * @params   config
+     * @author   Jenson
+     * @blog    http://jenson.gg/
+     * @version 2.0.0
+     * @date    2024-12-25
+     * @return  array
      */
-    public function __construct()
+    public function __construct($config = [])
     {
-        //
+        self::$url_v2 = 'https://v2.alapi.cn';
+        self::$api_v2 = '/api/exchange';
+        $currency_token = getenv('CURRENCY_TOKEN');
+        if(!empty($currency_token)){
+            self::$token_v2 = $currency_token;
+        }
     }
   
     /**
@@ -54,6 +74,11 @@ class CurrencyService
      */
     public function getCurrencyRates($params = [])
     {
+        $version = $params['version']??self::$version;
+        if($version == '2.0'){
+            $data = $this->getCurrencyRatesV2($params);
+            return $data;
+        }
         $url = self::$url;
         $api = self::$api;
         $amount = $params['amount']??100;
@@ -94,6 +119,61 @@ class CurrencyService
         return Helper::DataReturn('ok',200,$data);
     }
 
+    /**
+     * 获取汇率V2
+     * @params   amount:默认100,from：默认美元,to：默认人民币
+     * @author   Jenson
+     * @blog    http://jenson.gg/
+     * @version 2.0.0
+     * @date    2024-12-25
+     * @return  array
+     */
+    public function getCurrencyRatesV2($params = []){
+        $url = self::$url_v2;
+        $api = self::$api_v2;
+        $token = self::$token_v2;
+        $amount = $params['amount']??100;
+        #正向汇率
+        $from = $params['from']??'USD';
+        $to = $params['to']??'CNY';
+        $apiUrl = $url.$api.'?money='.$amount.'&from='.$from.'&to='.$to.'&token='.$token;
+        $result = file_get_html($apiUrl)->plaintext;
+        $result = json_decode($result,true);
+        $res = [];
+        if($result['code'] == 200){
+            $data = $result['data'];
+            $desc = $amount.$data["currency_form_name"].'可兑换'.$data["exchange_round"].$data["currency_to_name"];
+            $rate = $data['exchange']/$amount;
+            $res_forward = [
+                'desc'=>$desc,
+                'rate'=>$rate
+            ];
+            $res['forward'] = $res_forward;
+        }else{
+            return Helper::DataReturn($data['msg'],$data['code']);
+        }
+        #接口不支持并发
+        sleep(1);
+        #逆向汇率
+        $from = $params['to']??'CNY';
+        $to = $params['from']??'USD';
+        $apiUrl = $url.$api.'?money='.$amount.'&from='.$from.'&to='.$to.'&token='.$token;
+        $result = file_get_html($apiUrl)->plaintext;
+        $result = json_decode($result,true);
+        if($result['code'] == 200){
+            $data = $result['data'];
+            $desc = $amount.$data["currency_form_name"].'可兑换'.$data["exchange_round"].$data["currency_to_name"];
+            $rate = $data['exchange']/$amount;
+            $res_reverse = [
+                'desc'=>$desc,
+                'rate'=>$rate
+            ];
+            $res['reverse'] = $res_reverse;
+        }else{
+            return Helper::DataReturn($data['msg'],$data['code']);
+        }
+        return Helper::DataReturn('ok',200,$res);
+    }
     /**
      * 获取货币列表
      * @params   page,page_size
